cf8de3a395e2f742371525aca54462d8eb7f9cb8
Geam Santos <geam.felipe@gmail.com>
2023-02-05 18:36:18 -0300

    1º Commit

---
+++ PasseioCavaloFB/src/passeiocavalofb/PasseioCavaloFB.java
@@ -0,0 +1,18 @@
+package passeiocavalofb;
+
+/**
+ *
+ * @author Geam
+ */
+public class PasseioCavaloFB {
+
+  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new RealizaPasseio().executar();
+    }
+
+
+}
